f128918306d5f8287df1bdf4452f926307635e8d
Khalil Merine <merinekh@gmail.com>
2022-12-07 00:45:31 -0500

    Array object shows info setup

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,29 @@
+let shows = [
+    {
+        date : "Mon Sept 06 2021",
+        venue : "Ronald Lane",
+    },
+    {
+        date : "Tue Sept 11 2021",
+        venue : "Pier 3 East",
+    },
+    {
+        date : "Fri Oct 15 2021",
+        venue : "View Lounge",
+    },
+    {
+        date : "Sat Nov 06 2021",
+        venue : "Hyatt Agency",
+    },
+    {
+        date : "Fri Nov 26 2021",
+        venue : "Moscow Center",
+    },
+    {
+        date : "Wed Dec 15 2021",
+        venue : "Press Club",
+    }
+]
+
+shows.location = "San Francisco, Ca"
+console.log(shows)
