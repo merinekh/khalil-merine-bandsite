8b958a80021a39163f07072f1c931aad0b5cd5e1
Khalil Merine <merinekh@gmail.com>
2022-12-07 19:09:12 -0500

    Object variable array dates,venues,locations

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,29 +1,27 @@
-let shows = [
-    {
-        date : "Mon Sept 06 2021",
-        venue : "Ronald Lane",
-    },
-    {
-        date : "Tue Sept 11 2021",
-        venue : "Pier 3 East",
-    },
-    {
-        date : "Fri Oct 15 2021",
-        venue : "View Lounge",
-    },
-    {
-        date : "Sat Nov 06 2021",
-        venue : "Hyatt Agency",
-    },
-    {
-        date : "Fri Nov 26 2021",
-        venue : "Moscow Center",
-    },
-    {
-        date : "Wed Dec 15 2021",
-        venue : "Press Club",
-    }
-]
 
-shows.location = "San Francisco, Ca"
+let shows = {
+    date : ["Mon Sept 06 2021","Tue Sept 11 2021","Tue Sept 11 2021","Fri Oct 15 2021","Sat Nov 06 2021","Fri Nov 26 2021","Wed Dec 15 2021"],
+    venue : ["Ronald Lane","Pier 3 East","View Lounge","Hyatt Agency","Moscow Center","Press Club"],
+    location : ["San Francisco, Ca","San Francisco, Ca","San Francisco, Ca","San Francisco, Ca","San Francisco, Ca","San Francisco, Ca"]
+}
+
 console.log(shows)
+
+
+for (let i = 0; i < pets.length; i++) {
+    const petItem = pets[i];
+  
+    let pet = createElementWithClass("div", "pet");
+  
+    let petName = document.createElement("h3");
+    petName.classList.add("pet__name");
+    petName.innerText = petItem.name;
+  
+    let petBio = document.createElement("p");
+    petBio.classList.add("pet__bio");
+    petBio.innerText = petItem.bio;
+  
+    pet.appendChild(petName);
+    pet.appendChild(petBio);
+    petsContainer.appendChild(pet);
+  }
