d2274948a5cef7526fc0c16afa59d52cf215b65f
Khalil Merine <merinekh@gmail.com>
2022-12-09 22:31:26 -0500

    Main form components name  modif, Still having problem with rendering all cards

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -28,14 +28,17 @@ const commentContent = [
   },
 ];
 
-console.log(commentContent);
+// console.log(commentContent);
 const form = document.getElementsByClassName("form")[0];
 
 const formTitle = createElementWithClass("h2", "form__title");
 const formUp = createElementWithClass("div", "form-up");
 const formUpImage = createElementWithClass("img", "form-up__image");
 const formUpInputs = createElementWithClass("div", "form-up-inputs");
-const formSubtitleName = createElementWithClass("p", "form-subtitle__name");
+const formUpSubtitleName = createElementWithClass(
+  "p",
+  "form-up-subtitle__name"
+);
 const formUpInputsName = createElementWithClass(
   "input",
   "form-up-inputs__name"
@@ -63,25 +66,60 @@ const formLine = createElementWithClass("hr", "form__line");
 
 formTitle.innerHTML = "Join the Convesation";
 formCommentsImage.alt = "Profile-Img";
-formSubtitleName.innerHTML = "NAME";
+formUpSubtitleName.innerHTML = "NAME";
 formSubtitleComment.innerHTML = "COMMENT";
 formUpInputsName.placeholder = "Enter your name";
 formUpInputsComment.placeholder = "Add a new comment";
 formUpButton.type = "button";
 formUpButton.value = "COMMENT";
 formUpButton.classList.add("button");
-formSubtitleName.classList.add("form-subtitle");
+formUpSubtitleName.classList.add("form-subtitle");
 formSubtitleComment.classList.add("form-subtitle");
 
-for (let i = 0; i < commentContent.length; i++) {
-  let element = commentContent[i];
-  formProfileName.innerText = commentContent[i].author;
+// for (let i = 0; i < commentContent.length; i++) {
+//   let element = commentContent[i];
+//   formProfileName.innerText = element.author;
+//   formProfileDate.innerHTML = element.date;
+//   formComment.innerHTML = element.comment;
+//   // formCommentsImage.src = element.src;
+//   // console.log(element.date);
+//   // We've reference the specific element in the array, We've added a date,profilename,innerhtml,formcomment
+//   formUpInputs.appendChild(formUpSubtitleName);
+//   formUpInputs.appendChild(formUpInputsName);
+//   formUpInputs.appendChild(formSubtitleComment);
+//   formUpInputs.appendChild(formUpInputsComment);
+//   formUpInputs.appendChild(formUpButton);
+
+//   formUp.appendChild(formUpImage);
+//   formUp.appendChild(formUpInputs);
+
+//   formProfile.appendChild(formProfileName);
+//   formProfile.appendChild(formProfileDate);
+//   // console.log(formProfile);
+//   formCommentsInputs.appendChild(formProfile);
+//   formCommentsInputs.appendChild(formComment);
+
+//   formComments.appendChild(formCommentsImage);
+//   formComments.appendChild(formCommentsInputs);
+
+//   form.appendChild(formTitle);
+//   form.appendChild(formUp);
+//   form.appendChild(formComments);
+//   form.appendChild(formLine);
+//   // console.log(formComments);
+//   // console.log(element);
+// }
+
+// // console.log(form);
+
+commentContent.forEach((element) => {
+  formProfileName.innerText = element.author;
   formProfileDate.innerHTML = element.date;
   formComment.innerHTML = element.comment;
   // formCommentsImage.src = element.src;
-  console.log(element.date);
+  // console.log(element.date);
   // We've reference the specific element in the array, We've added a date,profilename,innerhtml,formcomment
-  formUpInputs.appendChild(formSubtitleName);
+  formUpInputs.appendChild(formUpSubtitleName);
   formUpInputs.appendChild(formUpInputsName);
   formUpInputs.appendChild(formSubtitleComment);
   formUpInputs.appendChild(formUpInputsComment);
@@ -103,8 +141,5 @@ for (let i = 0; i < commentContent.length; i++) {
   form.appendChild(formUp);
   form.appendChild(formComments);
   form.appendChild(formLine);
-  // console.log(formComments);
-  // console.log(element);
-}
-
-// console.log(form);
+  console.log(element);
+});
