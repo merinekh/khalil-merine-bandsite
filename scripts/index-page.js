d226538b172b6e4a7168f71aeb759fd44e903717
Khalil Merine <merinekh@gmail.com>
2022-12-09 22:13:12 -0500

    Main form styling done + Still having problem with rendering all cards

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -33,19 +33,22 @@ const form = document.getElementsByClassName("form")[0];
 
 const formTitle = createElementWithClass("h2", "form__title");
 const formUp = createElementWithClass("div", "form-up");
-const formImage = createElementWithClass("img", "form__image");
-const formInputs = createElementWithClass("div", "form-inputs");
+const formUpImage = createElementWithClass("img", "form-up__image");
+const formUpInputs = createElementWithClass("div", "form-up-inputs");
 const formSubtitleName = createElementWithClass("p", "form-subtitle__name");
-const formInputsName = createElementWithClass("input", "form-inputs__name");
+const formUpInputsName = createElementWithClass(
+  "input",
+  "form-up-inputs__name"
+);
 const formSubtitleComment = createElementWithClass(
   "p",
   "form-subtitle__comment"
 );
-const formInputsComment = createElementWithClass(
+const formUpInputsComment = createElementWithClass(
   "input",
-  "form-inputs__comment"
+  "form-up-inputs__comment"
 );
-const formButton = createElementWithClass("input", "form__button");
+const formUpButton = createElementWithClass("input", "form-up__button");
 const formComments = createElementWithClass("div", "form-comments");
 const formCommentsImage = createElementWithClass("img", "form-comments__image");
 const formCommentsInputs = createElementWithClass(
@@ -56,12 +59,17 @@ const formComment = createElementWithClass("p", "form__comment");
 const formProfile = createElementWithClass("div", "form-profile");
 const formProfileName = createElementWithClass("p", "form-profile__name");
 const formProfileDate = createElementWithClass("p", "form-profile__date");
+const formLine = createElementWithClass("hr", "form__line");
 
 formTitle.innerHTML = "Join the Convesation";
+formCommentsImage.alt = "Profile-Img";
 formSubtitleName.innerHTML = "NAME";
 formSubtitleComment.innerHTML = "COMMENT";
-formButton.placeholder = "COMMENT";
-formButton.classList.add("button");
+formUpInputsName.placeholder = "Enter your name";
+formUpInputsComment.placeholder = "Add a new comment";
+formUpButton.type = "button";
+formUpButton.value = "COMMENT";
+formUpButton.classList.add("button");
 formSubtitleName.classList.add("form-subtitle");
 formSubtitleComment.classList.add("form-subtitle");
 
@@ -70,17 +78,17 @@ for (let i = 0; i < commentContent.length; i++) {
   formProfileName.innerText = commentContent[i].author;
   formProfileDate.innerHTML = element.date;
   formComment.innerHTML = element.comment;
-  formCommentsImage.src = element.src;
+  // formCommentsImage.src = element.src;
   console.log(element.date);
   // We've reference the specific element in the array, We've added a date,profilename,innerhtml,formcomment
-  formInputs.appendChild(formSubtitleName);
-  formInputs.appendChild(formInputsName);
-  formInputs.appendChild(formSubtitleComment);
-  formInputs.appendChild(formInputsComment);
-  formInputs.appendChild(formButton);
+  formUpInputs.appendChild(formSubtitleName);
+  formUpInputs.appendChild(formUpInputsName);
+  formUpInputs.appendChild(formSubtitleComment);
+  formUpInputs.appendChild(formUpInputsComment);
+  formUpInputs.appendChild(formUpButton);
 
-  formUp.appendChild(formImage);
-  formUp.appendChild(formInputs);
+  formUp.appendChild(formUpImage);
+  formUp.appendChild(formUpInputs);
 
   formProfile.appendChild(formProfileName);
   formProfile.appendChild(formProfileDate);
@@ -93,7 +101,8 @@ for (let i = 0; i < commentContent.length; i++) {
 
   form.appendChild(formTitle);
   form.appendChild(formUp);
-  form.append(formComments);
+  form.appendChild(formComments);
+  form.appendChild(formLine);
   // console.log(formComments);
   // console.log(element);
 }
