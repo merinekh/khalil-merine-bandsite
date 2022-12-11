4e7613bf8199a0e345bb83f62df77c02dc915389
Khalil Merine <merinekh@gmail.com>
2022-12-11 01:37:00 -0500

    Display New Comment Main-Components

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -10,21 +10,21 @@ const commentContent = [
     comment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
     date: "02/17/2021",
-    src: "",
+    src: "https://i.pravatar.cc/150?img=3",
   },
   {
     author: "Emilie Beach",
     comment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What   an incredible day.",
     date: "01/09/2021",
-    src: "",
+    src: "https://i.pravatar.cc/150?img=5",
   },
   {
     author: "Miles Acosta",
     comment:
       "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
     date: "12/20/2020",
-    src: "",
+    src: "https://i.pravatar.cc/150?img=6",
   },
 ];
 
@@ -134,7 +134,90 @@ commentContent.forEach((element) => {
 
   addCard();
 
-  // formCommentsImage.src = element.src;
+  formCommentsImage.src = element.src;
   // console.log(element.date);
   // console.log(element);
 });
+
+// ------------------DISPLAY NEW COMMENT---------------
+// console.log(formUpButton.click);
+formUpButton.addEventListener("click", (event) => {
+  const newName = formUpInputsName.value;
+  const newComment = formUpInputsComment.value;
+  const newImage = formUpImage.src;
+  const newDate = new Date().toLocaleDateString();
+
+  let newObj = [
+    {
+      author: newName,
+      comment: newComment,
+      date: newDate,
+      src: newImage,
+    },
+  ];
+
+  commentContent.unshift(newObj);
+  console.log(newObj, commentContent);
+
+  // alert("Submitted");
+
+  function addCard() {
+    const formComments = createElementWithClass("div", "form-comments");
+    const formCommentsImage = createElementWithClass(
+      "img",
+      "form-comments__image"
+    );
+    const formCommentsInputs = createElementWithClass(
+      "div",
+      "form-comments-inputs"
+    );
+    const formComment = createElementWithClass("p", "form__comment");
+    const formProfile = createElementWithClass("div", "form-profile");
+    const formProfileName = createElementWithClass("p", "form-profile__name");
+    const formProfileDate = createElementWithClass("p", "form-profile__date");
+    const formLine = createElementWithClass("hr", "form__line");
+
+    return;
+  }
+});
+
+// newObj.forEach((element) => {
+//   const formComments = createElementWithClass("div", "form-comments");
+//   const formCommentsImage = createElementWithClass(
+//     "img",
+//     "form-comments__image"
+//   );
+//   const formCommentsInputs = createElementWithClass(
+//     "div",
+//     "form-comments-inputs"
+//   );
+//   const formComment = createElementWithClass("p", "form__comment");
+//   const formProfile = createElementWithClass("div", "form-profile");
+//   const formProfileName = createElementWithClass("p", "form-profile__name");
+//   const formProfileDate = createElementWithClass("p", "form-profile__date");
+//   const formLine = createElementWithClass("hr", "form__line");
+
+//   formCommentsImage.alt = "Profile-Img";
+
+//   formProfile.appendChild(formProfileName);
+//   formProfile.appendChild(formProfileDate);
+//   // console.log(formProfile);
+//   formCommentsInputs.appendChild(formProfile);
+//   formCommentsInputs.appendChild(formComment);
+
+//   formComments.appendChild(formCommentsImage);
+//   formComments.appendChild(formCommentsInputs);
+
+//   form.appendChild(formComments);
+//   form.appendChild(formLine);
+
+//   formProfileName.innerText = element.author;
+//   formProfileDate.innerHTML = element.date;
+//   formComment.innerHTML = element.comment;
+
+//   addCard();
+
+//   formCommentsImage.src = element.src;
+//   // console.log(element.date);
+//   // console.log(element);
+// });
