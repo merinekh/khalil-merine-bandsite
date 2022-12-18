8b394f975694b80f3451ed4114eed630dbfe2a98
Khalil Merine <merinekh@gmail.com>
2022-12-17 20:27:04 -0500

    Get part 95% Done

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,29 +4,24 @@ const createElementWithClass = (type, className) => {
   return element;
 };
 
-const commentContent = [
-  {
-    author: "Connor Walton",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    date: "02/17/2021",
-    src: "https://i.pravatar.cc/150?img=3",
-  },
-  {
-    author: "Emilie Beach",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What   an incredible day.",
-    date: "01/09/2021",
-    src: "https://i.pravatar.cc/150?img=5",
-  },
-  {
-    author: "Miles Acosta",
-    comment:
-      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
-    date: "12/20/2020",
-    src: "https://i.pravatar.cc/150?img=6",
-  },
-];
+// -----------------------------WEB API--------------------------
+const usersURL = "https://project-1-api.herokuapp.com/";
+const myKey = "?api_key=c71dd9a6-0d71-4306-b904-6cea084f1404";
+const myPage = ["register", "comments", "showdates"];
+const api = `${usersURL}${myPage[1]}${myKey}`;
+// console.log(api);
+const CommentContent = axios.get(api).then((response) => {
+  data = response.data;
+  // console.log(data);
+  data.forEach((element) => {
+    addCard(element);
+    // console.log(element.date);
+    // console.log(element);
+  });
+});
+console.log(CommentContent);
+// ----------------------------------------------------------------
+let commentContent = [];
 
 // -----------------SELECT FORM----------------------
 const form = document.getElementsByClassName("form-comments")[0];
@@ -67,8 +62,8 @@ addCard = (element) => {
   form.appendChild(formComments);
   form.appendChild(formLine);
 
-  formProfileName.innerText = element.author;
-  formProfileDate.innerHTML = element.date;
+  formProfileName.innerText = element.name;
+  formProfileDate.innerHTML = element.timestamp;
   formComment.innerHTML = element.comment;
   formCommentsImage.src = element.src;
 };
@@ -76,10 +71,15 @@ addCard = (element) => {
 // ------PASSING THRU EACH ELEMENT OF THE COMMENTCONTENT ARRAY------
 commentContent.forEach((element) => {
   addCard(element);
-  // console.log(element.date);
   // console.log(element);
 });
-
+function loadComments(comments) {
+  comments.forEach((element) => {
+    addCard(element);
+    // console.log(element.date);
+    // console.log(element);
+  });
+}
 // -------------Event Listener-----------
 const itemForm = document.getElementsByClassName("section-form-main")[0];
 
@@ -96,7 +96,7 @@ itemForm.addEventListener("submit", function (event) {
   commentContent.forEach((element) => {
     deleteComments();
   });
-  // --------------------Get inputs---------
+  // --------------------Get inputs & into variable: newObj---------
   let newName = document.getElementsByClassName("section-form-inputs__name")[0]
     .value;
   let newComment = document.getElementsByClassName(
@@ -116,8 +116,6 @@ itemForm.addEventListener("submit", function (event) {
   commentContent.sort((a, b) => new Date(b.date) - new Date(a.date));
   // console.log(commentContent);
 
-  // console.log(addCard(commentContent));
-
   // // ------------------APPLY CODE FOR NEW ARRAY COMMENTCONTENT-------------------------------------------------
   commentContent.forEach((element) => {
     addCard(element);
@@ -125,54 +123,3 @@ itemForm.addEventListener("submit", function (event) {
     // console.log(addCard(element));
   });
 });
-
-// // ------------------APPLY CODE FOR NEW ARRAY COMMENTCONTENT-------------------------------------------------
-// newCommentContent.forEach((element) => {
-//   // addCard(element);
-//   function addCard(element) {
-//     const formComments = createElementWithClass(
-//       "div",
-//       "section-form-comments"
-//     );
-//     const formCommentsImage = createElementWithClass(
-//       "img",
-//       "section-form-comments__image"
-//     );
-//     const formCommentsInputs = createElementWithClass(
-//       "div",
-//       "section-form-comments-inputs"
-//     );
-//     const formComment = createElementWithClass("p", "form__comment");
-//     const formProfile = createElementWithClass("div", "section-form-profile");
-//     const formProfileName = createElementWithClass(
-//       "p",
-//       "section-form-profile__name"
-//     );
-//     const formProfileDate = createElementWithClass(
-//       "p",
-//       "section-form-profile__date"
-//     );
-//     const formLine = createElementWithClass("hr", "section-form__line");
-
-//     formCommentsImage.alt = "Profile-Img";
-
-//     formProfile.appendChild(formProfileName);
-//     formProfile.appendChild(formProfileDate);
-//     // console.log(formProfile);
-//     formCommentsInputs.appendChild(formProfile);
-//     formCommentsInputs.appendChild(formComment);
-
-//     formComments.appendChild(formCommentsImage);
-//     formComments.appendChild(formCommentsInputs);
-//     // console.log(formComments);
-//     form.appendChild(formComments);
-//     form.appendChild(formLine);
-//     console.log(form);
-//     formProfileName.innerText = element.author;
-//     formProfileDate.innerHTML = element.date;
-//     formComment.innerHTML = element.comment;
-//     formCommentsImage.src = element.src;
-//   }
-//   // console.log(element.date);
-//   // console.log(element);
-// });
